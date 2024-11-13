9840c7141c69dc442e72e68e16823a8df8f2f4ed
Saint-Preux <rsaintpreux@mesfire.com>
2024-11-13 09:04:52 -0500

    pics

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
